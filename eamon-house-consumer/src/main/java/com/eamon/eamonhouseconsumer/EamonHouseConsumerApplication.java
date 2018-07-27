1cd0c47a0d9c0aca10de593e14a962ef33beeb2e
eamonzhang <329294892@qq.com>
2018-07-27 14:04:12 +0800

    添加 服务消费 采用restTemplate方式

--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/EamonHouseConsumerApplication.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/EamonHouseConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.eamon.eamonhouseservice;
+package com.eamon.eamonhouseconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EamonHouseServiceApplication {
+public class EamonHouseConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EamonHouseServiceApplication.class, args);
+        SpringApplication.run(EamonHouseConsumerApplication.class, args);
     }
+
 }
---
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
@@ -0,0 +1,20 @@
+package com.eamon.eamonhouseconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/consumer")
+public class SubtitutionController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/callHello")
+    public String callHello(){
+        return restTemplate.getForObject("http://localhost:8081/house/hello", String.class);
+    }
+
+}
