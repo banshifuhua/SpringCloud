ac9c0c7d84bb97f11e5ec7fd2159a1762c34b47d
EAMON\Eamon <329294892@qq.com>
2018-07-29 23:18:57 +0800

    house-service 添加ribbon 实现负载均衡 启动两个端口8081,8083  并在 consumer中使用callHello接口进行了验证 http://localhost:8082/consumer/callHello  会出现8081和8083接口交替访问

--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
@@ -1,9 +1,8 @@
 package com.eamon.eamonhouseconsumer.controller;
 
+import com.eamon.eamonhouseservice.po.HouseInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -13,8 +12,20 @@ public class SubtitutionController {
     RestTemplate restTemplate;
 
     @GetMapping("/callHello")
-    public String callHello(){
-        return restTemplate.getForObject("http://house-service/house/hello", String.class);
+    public String callHello() {
+        String result = restTemplate.getForObject("http://house-service/house/hello", String.class);
+        System.out.println("调用结果：" + result);
+        return result;
+    }
+
+    @GetMapping("/data")
+    public HouseInfo getData(@RequestParam("name") String name) {
+        return restTemplate.getForObject("http://house-service/house/data?name=" + name, HouseInfo.class);
+    }
+
+    @GetMapping("/data/{name}")
+    public String getData2(@PathVariable("name") String name) {
+        return restTemplate.getForObject("http://house-service/house/data/{name}", String.class, name);
     }
 
 }
--- eamon-house-service/src/main/java/com/eamon/eamonhouseservice/controller/HouseController.java
+++ eamon-house-service/src/main/java/com/eamon/eamonhouseservice/controller/HouseController.java
@@ -1,16 +1,36 @@
 package com.eamon.eamonhouseservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eamon.eamonhouseservice.po.HouseInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/house")
 public class HouseController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        return "Hello" + serverPort;
     }
 
+    @GetMapping("/data")
+    public HouseInfo getData(@RequestParam("name") String name) {
+        HouseInfo houseInfo = new HouseInfo();
+        houseInfo.setId(1L);
+        houseInfo.setCity("上海");
+        houseInfo.setRegion("虹口");
+        houseInfo.setName("东体小区");
+        return houseInfo;
+    }
+
+    @GetMapping("/data/{name}")
+    public String getData2(@PathVariable("name") String name) {
+        return name;
+    }
 }
