723b74a57f3acf7f6163b139b3da93b80feff226
eamonzhang <329294892@qq.com>
2018-07-30 09:07:28 +0800

    add datasource setting

---
+++ eamon-eureka-slaveone/src/main/java/com/eamon/eamoneureka/EamonEurekaSlaveoneApplication.java
@@ -0,0 +1,14 @@
+package com.eamon.eamoneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EamonEurekaSlaveoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EamonEurekaSlaveoneApplication.class, args);
+    }
+}
