70816a1c87ad77a8d4c0dcaadf72b2327d6c247b
eamon <329294892@qq.com>
2018-08-01 09:55:30 +0800

    feign 超时时间配置

--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/conf/FeignConfiguration.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/conf/FeignConfiguration.java
@@ -1,6 +1,7 @@
 package com.eamon.eamonhouseconsumer.conf;
 
 import feign.Logger;
+import feign.Request;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,22 @@ public class FeignConfiguration {
         return Logger.Level.FULL;
     }
 
+    /**
+     * basic认证
+     */
 //    @Bean
 //    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
 //        return new BasicAuthRequestInterceptor("user", "password");
 //    }
+
+    /**
+     * 超时时间配置
+     *  连接超时，读取超时  毫秒
+     * @return
+     */
+    @Bean
+    public Request.Options options(){
+        return new Request.Options(5000, 10000);
+    }
 
 }
