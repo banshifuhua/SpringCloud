d6b844ec7431d98c7d1ecff10219815d33a0b7ff
eamon <329294892@qq.com>
2018-08-01 10:22:14 +0800

    hystrix 其他属性配置示例 隔离策略-线程隔离

--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
@@ -3,7 +3,9 @@ package com.eamon.eamonhouseconsumer.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.eamon.eamonhouseconsumer.common.CallHelloFallback;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +15,8 @@ public class SubtitutionController {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "defaultCallHello")
+    @HystrixCommand(fallbackMethod = "defaultCallHello"
+            , commandProperties = {@HystrixProperty(name = "execution.isolation.strategy", value = "THREAD")})
     @GetMapping("/callHello")
     public String callHello() {
         String result = restTemplate.getForObject("http://house-service/house/hello", String.class);
