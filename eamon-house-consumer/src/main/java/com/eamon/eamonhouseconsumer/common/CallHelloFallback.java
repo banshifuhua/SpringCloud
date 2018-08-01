61e51ad879036ddd1c396b0e4721fefb547d562a
eamon <329294892@qq.com>
2018-08-01 10:17:13 +0800

    添加 hystrix 并简单使用

---
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/common/CallHelloFallback.java
@@ -0,0 +1,7 @@
+package com.eamon.eamonhouseconsumer.common;
+
+public class CallHelloFallback {
+    public static String defaultCallHello() {
+        return "fail";
+    }
+}
--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/controller/SubtitutionController.java
@@ -1,6 +1,8 @@
 package com.eamon.eamonhouseconsumer.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.eamon.eamonhouseconsumer.common.CallHelloFallback;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ public class SubtitutionController {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "defaultCallHello")
     @GetMapping("/callHello")
     public String callHello() {
         String result = restTemplate.getForObject("http://house-service/house/hello", String.class);
@@ -26,6 +29,11 @@ public class SubtitutionController {
     @GetMapping("/data/{name}")
     public String getData2(@PathVariable("name") String name) {
         return restTemplate.getForObject("http://house-service/house/data/{name}", String.class, name);
+    }
+
+
+    public String defaultCallHello() {
+        return "fail";
     }
 
 }
