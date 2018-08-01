1b4a7fc7c63fa7b31fbae64a239503f19a8e1106
eamon <329294892@qq.com>
2018-08-01 09:51:58 +0800

    暂时没懂 basic认证

--- eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/conf/FeignConfiguration.java
+++ eamon-house-consumer/src/main/java/com/eamon/eamonhouseconsumer/conf/FeignConfiguration.java
@@ -1,6 +1,7 @@
 package com.eamon.eamonhouseconsumer.conf;
 
 import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,4 +12,10 @@ public class FeignConfiguration {
     Logger.Level logger() {
         return Logger.Level.FULL;
     }
+
+//    @Bean
+//    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
+//        return new BasicAuthRequestInterceptor("user", "password");
+//    }
+
 }
